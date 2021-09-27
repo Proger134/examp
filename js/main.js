e435cf4a661204d1e1673f2936977e57f1030175
AA <74669025+Proger134@users.noreply.github.com>
2021-09-27 17:55:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -25,24 +25,22 @@ $(document).ready(function () {
   });
 }); // Bottom hover
 
-if (window.innerWidth > 480) {
-  $(".first-screen-slider__bottom").hover(function () {
-    var leftCss = {
-      transform: "translateX(-500%)"
-    };
-    var rightCss = {
-      transform: "translateX(500%)"
-    };
-    $(".first-screen-slider-arrow-left").css(leftCss);
-    $(".first-screen-slider-arrow-right").css(rightCss);
-  }, function () {
-    var UnCss = {
-      transform: "translateX(0)"
-    };
-    $(".first-screen-slider-arrow-left").css(UnCss);
-    $(".first-screen-slider-arrow-right").css(UnCss);
-  });
-}
+$(".first-screen-slider__bottom").hover(function () {
+  var leftCss = {
+    transform: "translateX(-500%)"
+  };
+  var rightCss = {
+    transform: "translateX(500%)"
+  };
+  $(".first-screen-slider-arrow-left").css(leftCss);
+  $(".first-screen-slider-arrow-right").css(rightCss);
+}, function () {
+  var UnCss = {
+    transform: "translateX(0)"
+  };
+  $(".first-screen-slider-arrow-left").css(UnCss);
+  $(".first-screen-slider-arrow-right").css(UnCss);
+});
 "use strict";
 
 var validateForms = function validateForms(selector, rules, successModal, yaGoal) {
